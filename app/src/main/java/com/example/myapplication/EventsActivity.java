9de2164cc5c27299580d7aa74830e491ac0ec157
Harnoor <hsingh9@student.gsu.edu>
2019-03-30 19:00:40 -0400

    merged Yasir Changes

--- app/src/main/java/com/example/myapplication/EventsActivity.java
+++ app/src/main/java/com/example/myapplication/EventsActivity.java
@@ -1,15 +1,17 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class EventsActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener{
+public class EventsActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
     MyRecyclerViewAdapter adapter;
 
@@ -34,6 +36,14 @@ public class EventsActivity extends AppCompatActivity implements MyRecyclerViewA
         adapter = new MyRecyclerViewAdapter(this, animalNames);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
+
+        Button btnShare = findViewById(R.id.btnShare);
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ShareActivity.class));
+            }
+        });
     }
 
     @Override
